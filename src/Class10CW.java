7018db2ae8c868887594dd9a76042be64b8397b3
pranav6209 <pranav.6209@gmail.com>
2017-01-24 10:14:44 -0800

    Changes after Class 9 and Class 10
    Format changes

--- src/Class10CW.java
+++
@@ -1,4 +0,0 @@
-
-public class Class10CW {
-// Inside Class-9
-}
--- src/Class10HWTest.java
+++ src/Class10HWTest.java
@@ -17,9 +17,5 @@ public class Class10HWTest {
 		System.out.println("Product Id" + pr.id + "\nProduct Name:" + pr.name
 				+ "\nProduct Price" + pr.price);
 
-		// System.out.println("Product Id" + pr.id);
-		// System.out.println("Product Name:" + pr.name);
-		// System.out.println("Product Price" + pr.price);
-
 	}
 };
